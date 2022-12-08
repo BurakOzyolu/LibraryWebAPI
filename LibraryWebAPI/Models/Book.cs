62a39db898a6639febff2ed912d6893fadd8c5dc
BurakOzyolu <burakozyolu@gmail.com>
2022-12-08 23:06:41 +0300

    BookClass Added

---
+++ LibraryWebAPI/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryWebAPI.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; }
+        public int AuthorId { get; set; }
+        public int TypeId { get; set; }
+        public int Year { get; set; }
+    }
+}
