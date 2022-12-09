f9200bd90ac01603633ed67cc729e579981c7865
BurakOzyolu <burakozyolu@gmail.com>
2022-12-09 23:28:02 +0300

    added StudentViewModel

---
+++ LibraryWebAPI/ViewModels/StudentViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryWebAPI.ViewModels
+{
+    public class StudentViewModel
+    {
+        public int studentId { get; set; }
+        public string studentName { get; set; }
+        public string studentClass { get; set; }
+    }
+}
